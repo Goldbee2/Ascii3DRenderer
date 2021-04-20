ec4f0f0150ec956f99b06120b9d4c2675416cad2
Eli Goldberg <goldbee2@wwu.edu>
2021-04-19 18:30:51 -0700

    added .gitignore

--- Scene.py
+++ Scene.py
@@ -1,5 +1,5 @@
 # a collection of objects and their positions/rotations.
-#NOTE: WORLDSPACE IS RIGHT-HANDED (facing negative Z), and COUNTERCLOCKWISE
+#NOTE: WORLDSPACE IS RIGHT-HANDED (facing negative Z), and vertices are COUNTERCLOCKWISE
 class Scene:
     objects = []
     
@@ -7,7 +7,7 @@ class Scene:
         x, y, z = origin
         for triangle in object:
             for vertex in triangle:
-                vertex[0]+=x
-                vertex[1]+=y
-                vertex[2]+=z
+                vertex[0] += x
+                vertex[1] += y
+                vertex[2] += z
         self.objects.append(object)
