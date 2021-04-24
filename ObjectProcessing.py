4ab58057a1135e4a920a220cc84881e73eb9f0a4
Eli Goldberg <goldbee2@wwu.edu>
2021-04-23 21:44:35 -0700

    Added mesh scaling, translation, and bounding box.

--- ObjectProcessing.py
+++ ObjectProcessing.py
@@ -3,15 +3,55 @@ import array
 class Mesh:
     faces = []
     bounding_box = [0, 0, 0]
+    
+    
+    def scale_mesh(self, scale):
+        for face in self.faces:
+            for vertex in face:
+                for i in range(len(vertex)):
+                    vertex[i] *= scale
+        self.create_bounding_box()
 
 
 
-#FIXME does not work
-def scale_mesh(mesh, scale):
-    for face in mesh.faces:
-        for vertex in face:
-            for coordinate in vertex:
-                coordinate *= scale
+    def translate_mesh(self, translation):
+        for i in range(len(self.faces)):
+            for j in range(len(self.faces[i])):
+                self.faces[i][j] = (
+                self.faces[i][j][0] + translation[0],
+                self.faces[i][j][1] + translation[1],
+                self.faces[i][j][2] + translation[2])
+        self.create_bounding_box()
+
+
+    def create_bounding_box(self):
+        x_max = 0.0
+        y_max = 0.0
+        z_max = 0.0
+
+        x_min = 0.0
+        y_min = 0.0
+        z_min = 0.0
+
+        for face in self.faces:
+            for vertex in face:
+                if vertex[0] > x_max:
+                    x_max = vertex[0]
+                if vertex[1] > y_max:
+                    y_max = vertex[1]
+                if vertex[2] > z_max:
+                    z_max = vertex[2]
+                if vertex[0] < x_min:
+                    x_min = vertex[0]
+                if vertex[1] < y_min:
+                    y_min = vertex[1]
+                if vertex[2] < z_min:
+                    z_min = vertex[2]
+
+        max_coords = (x_max, y_max, z_max)
+        min_coords = (x_min, y_min, z_min)
+        self.bounding_box = (max_coords, min_coords)
+
 
 
 def parse_into_meshes(file='') :
@@ -52,34 +92,9 @@ def create_mesh(file, line, vertices):
             faces.append(line_faces)
         if line.startswith("o "):
              break
-    bounding_box = create_bounding_box(vertices)
     
     new_mesh.faces = faces
-    new_mesh.bounding_box = bounding_box
+    new_mesh.create_bounding_box()
     
     return new_mesh
 
-def create_bounding_box(vertices):
-    x_max = 0.0
-    y_max = 0.0
-    z_max = 0.0
-
-    x_min = 0.0
-    y_min = 0.0
-    z_min = 0.0
-
-    for vertex in vertices.values():
-        if vertex[0] > x_max:
-            x_max = vertex[0]
-        if vertex[1] > y_max:
-            y_max = vertex[1]
-        if vertex[2] > z_max:
-            z_max = vertex[2]
-        if vertex[0] < x_min:
-            x_min = vertex[0]
-        if vertex[1] < y_min:
-            y_min = vertex[1]
-        if vertex[2] < z_min:
-            z_min = vertex[2]
-
-    return (x_max, y_max, z_max)
